7c3e74663a423a0d1acd7276e22f350265a3e7e3
Sumit Shitole <sumitshitole1000@gmail.com>
2020-12-24 10:39:57 +0530

    Project Initialization

---
+++ JqueryDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JqueryDemo.Startup))]
+namespace JqueryDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
