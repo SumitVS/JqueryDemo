52d21a5d62229a3ccbc3617c240d6d7795d5c066
Sumit Shitole <sumitshitole1000@gmail.com>
2020-12-26 12:12:08 +0530

    Jquery Basics

---
+++ JqueryDemo/Views/Home/JQueryBasicController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryDemo.Views.Home
+{
+    public class JQueryBasicController : Controller
+    {
+        /// <summary>
+        /// Difference between Windows.load and document.ready
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowImageHeightWidth()
+        {
+            return View();
+        }
+        public ActionResult IdSelector()
+        {
+            return View();
+        }
+        public ActionResult ElementSelector()
+        {
+            return View();
+        }
+        public ActionResult ClassSelector()
+        {
+            return View();
+        }
+
+        public ActionResult AttributeSelector()
+        {
+            return View();
+        }
+    }
+}
